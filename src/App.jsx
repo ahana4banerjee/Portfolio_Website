39c0ee0577d86a50ff396885e05eb52692ed7f24
Ahana4Banerjee <banerjeeahana4@gmail.com>
2025-05-11 00:31:35 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/Toaster";
 
 function App() {
   return (
